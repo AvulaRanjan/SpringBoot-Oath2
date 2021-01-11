4e0b8ab6c9980172143bf67fa34dbcbfe2274c84
AvulaRanjan <avula.us@gmail.com>
2021-01-10 21:48:04 -0500

    Create new project

---
+++ SpringBoot-Oath2-server/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.model.Employee;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/user/getEmployeesList", produces = "application/json")
+    @ResponseBody
+    public List<Employee> getEmployeesList() {
+        List<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.setEmpId("Ranajn");
+        emp.setEmpName("ranjan");
+        employees.add(emp);
+        return employees;
+
+    }
+
+}
